1f893f3363d00a97068146223da67a863f6a7676
Chester Palen-Michel <cpalenmichel@brandeis.edu>
2023-12-10 14:09:36 -0500

    Fix inference gpt2

--- inference_gpt2.py
+++ inference_gpt2.py
@@ -5,8 +5,6 @@ from argparse import ArgumentParser
 from simpletransformers.language_generation import LanguageGenerationModel
 from tqdm import tqdm
 
-OUTPUT_PATTERN = re.compile(r'\"output\":(.*)')
-
 
 def run_inference():
 
@@ -23,10 +21,8 @@ def run_inference():
             example = json.loads(line)
             prompt = example['input'] + "<SEP>"
             sys_out = model.generate(prompt=prompt, args={"max_length": 128})
-            # match = OUTPUT_PATTERN.search(sys_out[0])
-            # example['model_output'] = match.group(1)
-            prompt_replacement = f'{prompt}", '
-            example['model_output'] = sys_out[0].replace(prompt_replacement, "").replace('"output: "', "")
+
+            example['model_output'] = sys_out[0].replace(prompt, "")
             print(json.dumps(example), file=out)
 
 
